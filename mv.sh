d8d42d00229fa8191378070ba240ce9911d1a164
Lucien Shui <lucien@lucien.ink>
2024-06-16 23:43:51 +0800

    Update mv.sh

--- mv.sh
+++ mv.sh
@@ -1,8 +1,9 @@
-IMG_PATH="/root/lucienshui.github.io/assets/img/posts"
+BASE_DIR="/root/blog"
+IMG_PATH="${BASE_DIR}/assets/img/posts"
 
-ssh blog 'rm -rf /root/lucienshui.github.io/_posts/*'
-scp -r _posts/* blog:/root/lucienshui.github.io/_posts/
+ssh blog "rm -rf ${BASE_DIR}/_posts/*"
+scp -r _posts/* blog:${BASE_DIR}/_posts/
 ssh blog "rm -rf ${IMG_PATH} && mkdir -p ${IMG_PATH}"
 scp -r assets/img/posts/* "blog:${IMG_PATH}/"
-ssh blog 'cd /root/lucienshui.github.io && bash build.sh'
-echo 'http://10.0.1.205:3000'
+ssh blog "cd ${BASE_DIR} && bash build.sh"
+echo 'http://10.0.6.105:3000'
