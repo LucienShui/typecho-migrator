f1b09f193cd558c956633d09cc3a61009d649bb6
Lucien Shui <lucien@lucien.ink>
2024-11-24 02:03:46 +0800

    Remove build.sh from mv.sh

--- mv.sh
+++ mv.sh
@@ -5,5 +5,3 @@ ssh blog "rm -rf ${BASE_DIR}/_posts/*"
 scp -r _posts/* blog:${BASE_DIR}/_posts/
 ssh blog "rm -rf ${IMG_PATH} && mkdir -p ${IMG_PATH}"
 scp -r assets/img/posts/* "blog:${IMG_PATH}/"
-ssh blog "cd ${BASE_DIR} && bash build.sh"
-echo 'http://10.0.6.105:3000'
